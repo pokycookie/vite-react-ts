cf727441413945212c8580c52df348aedf2a5d3e
황세웅 <0cookieboy0@gmail.com>
2024-08-09 15:45:17 +0900

    feat: eslint prettier 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,24 @@
 import js from '@eslint/js'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 export default tseslint.config({
-  extends: [js.configs.recommended, ...tseslint.configs.recommended],
+  extends: [
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    eslintPluginPrettierRecommended, // prettier 설정을 eslint 규칙으로 설정 + prettier와 충돌하는 rules를 꺼주는 역할, https://github.com/prettier/eslint-plugin-prettier
+  ],
+  // plugins: [],
   files: ['**/*.{ts,tsx}'],
   ignores: ['dist'],
   languageOptions: {
     ecmaVersion: 2020,
     globals: globals.browser,
   },
-  plugins: {},
   rules: {
     'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/no-unused-vars': 'warn',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
   },
 })
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
-  return <>Hello</>;
+  return <>Hello</>
 }
 
-export default App;
+export default App
