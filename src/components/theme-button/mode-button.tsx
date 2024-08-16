427ccf0373fc6fac3a22941a3b1811573a4234a8
pokycookie <0cookieboy0@gmail.com>
2024-08-16 22:20:38 +0900

    feat: mode-button

---
+++ src/components/theme-button/mode-button.tsx
@@ -0,0 +1,22 @@
+import { useMemo } from 'react'
+import { useThemeStore } from '@/stores'
+import { Iconify } from '@/components/iconify'
+
+import { IconButton } from '@mui/material'
+
+export function ModeButton() {
+  const { mode, toggle } = useThemeStore()
+
+  const icon = useMemo(() => {
+    if (mode === 'light') {
+      return 'solar:sun-bold'
+    }
+    return 'solar:moon-bold'
+  }, [mode])
+
+  return (
+    <IconButton onClick={toggle}>
+      <Iconify icon={icon} />
+    </IconButton>
+  )
+}
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,17 +1,16 @@
-import { useThemeStore } from '@/stores'
 import { Icon } from '@iconify/react/dist/iconify.js'
+import { ModeButton } from '@/components/theme-button'
 
 import { Button, Container, Typography } from '@mui/material'
 
 export default function App() {
-  const { toggle } = useThemeStore()
-
   return (
     <Container sx={{ p: 2 }}>
       <Typography variant="h2">안녕하세요!</Typography>
-      <Button onClick={toggle}>
+      <Button>
         <Icon icon="solar:airbuds-case-open-bold" width={20} height={20} />
       </Button>
+      <ModeButton />
     </Container>
   )
 }
